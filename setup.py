3ea6c3b662c34e87c9ebbc3c5f08dfa265d10933
Christian Buda <58868433+christianbuda@users.noreply.github.com>
2025-04-21 20:11:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mesh_poisson_disk_sampling',
-    version='1.0',
+    version='1.0.1',
     url='https://github.com/christianbuda/mesh_poisson_disk_sampling',
     author='Christian Buda',
     author_email='chrichri975@gmail.com',
